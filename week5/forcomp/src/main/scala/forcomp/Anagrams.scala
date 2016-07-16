daa899ec556102c8a5fdcb5f58269c8de06464c2
DedkovVA <VADedkov.SBT@sberbank.ru>
2016-07-16 20:59:30 +0300

    week 5 refactoring

--- week5/forcomp/src/main/scala/forcomp/Anagrams.scala
+++ week5/forcomp/src/main/scala/forcomp/Anagrams.scala
@@ -3,7 +3,7 @@ package forcomp
 import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
 
-
+/* todo scala api has methods 'combinations' and 'permutations'*/
 object Anagrams {
 
   /** A word is simply a `String`. */
@@ -38,7 +38,7 @@ object Anagrams {
    *  Note: you must use `groupBy` to implement this method!
    */
   def wordOccurrences(w: Word): Occurrences =
-    List(w.toLowerCase.toCharArray.groupBy((ch: Char) => ch).map(entry => (entry._1, entry._2.length)).toSeq.sortBy(_._1):_*)
+    List(w.toLowerCase.toCharArray.groupBy(ch => ch).map(e => (e._1, e._2.length)).toSeq.sortBy(_._1):_*)
 
   /** Converts a sentence into its character occurrence list. */
   def sentenceOccurrences(s: Sentence): Occurrences =
@@ -63,7 +63,8 @@ object Anagrams {
     dictionary.groupBy(word => wordOccurrences(word))
 
   /** Returns all the anagrams of a given word. */
-  def wordAnagrams(word: Word): List[Word] = dictionaryByOccurrences.get(wordOccurrences(word)).get
+  def wordAnagrams(word: Word): List[Word] =
+    dictionaryByOccurrences.get(wordOccurrences(word)).get
 
   /** Returns the list of all subsets of the occurrence list.
    *  This includes the occurrence itself, i.e. `List(('k', 1), ('o', 1))`
@@ -94,7 +95,8 @@ object Anagrams {
     var result = List[List[(Char, Int)]]()
 
     numToCharCombs.foreach(entry => entry._2.foreach(charList => {
-      //if occurrences = List((a, 2), (b, 2)) and charList = List(a) then charListCombinations = List((a, 1), (a, 2))
+      //for example: if occurrences = List((a, 2), (b, 2)) and charList = List(a)
+      // then charListCombinations = List((a, 1), (a, 2))
       var charListCombinations = List[List[(Char, Int)]](List[(Char, Int)]())
       charList.foreach(ch => {
         var swapCharListCombinations = List[List[(Char, Int)]]()
@@ -110,52 +112,50 @@ object Anagrams {
     result
   }
 
-  def sortWithOcc(occ1: Occurrences, occ2: Occurrences): Boolean = {
-    if (occ1.isEmpty && occ2.nonEmpty) true
-    else if (occ1.nonEmpty && occ2.isEmpty) false
-    else if (occ1.isEmpty && occ2.isEmpty) true
+  def sortTwoOccurrences(o1: Occurrences, o2: Occurrences): Boolean = {
+    if (o1.isEmpty && o2.nonEmpty) true
+    else if (o1.nonEmpty && o2.isEmpty) false
+    else if (o1.isEmpty && o2.isEmpty) true
     else {
-      val len = Math.min(occ1.length, occ2.length)
+      val len = Math.min(o1.length, o2.length)
       for (i <- 0 until len) {
-        if (occ1(i)._1 < occ2(i)._1) return true
-        else if (occ1(i)._1 > occ2(i)._1) return false
-        else {
-          if (occ1(i)._2 < occ2(i)._2) return true
-          else if (occ1(i)._2 > occ2(i)._2) return false
-        }
+        if (o1(i)._1 < o2(i)._1) return true
+        else if (o1(i)._1 > o2(i)._1) return false
+        else if (o1(i)._2 < o2(i)._2) return true
+        else if (o1(i)._2 > o2(i)._2) return false
       }
-      if (occ1.length <= occ2.length) true
-      else false
+      o1.length <= o2.length
     }
   }
 
   def sortOccurrencesList(occurrencesList: List[Occurrences]): List[Occurrences] = {
-    occurrencesList.sortWith((occ1, occ2) => sortWithOcc(occ1, occ2))
+    occurrencesList.sortWith((o1, o2) => sortTwoOccurrences(o1, o2))
   }
 
-  /**List((a, na), (b, nb), (c, nc)) --> Map(
+  /** For example:
+    * List((a, n_a), (b, n_b), (c, n_c)) --> Map(
     * 0 --> Set(List()),
     * 1 --> Set(List(a), List(b), List(c)),
     * 2 --> Set(List(a, b), List(b, c), List(a, c))
     * 3 --> Set(List(a, b, c))
     * )*/
   def charCombinations(occurrences: Occurrences): mutable.Map[Int, mutable.Set[List[Char]]] = {
-    val chars = occurrences.map(entry => entry._1)
+    val chars = occurrences.map(e => e._1)
     val numToCharCombs = mutable.Map[Int, mutable.Set[List[Char]]]((0, mutable.Set(List())))
 
     for (i <- 1 to chars.length) {
       numToCharCombs(i) = mutable.Set()
 
       val prevCombs = numToCharCombs(i - 1)
-      prevCombs.foreach(charList => {
-        chars.foreach(ch => {
+      prevCombs.foreach(charList =>
+        chars.foreach(ch =>
           if (charList.isEmpty || charList.last < ch) {
             val charListCandidate = charList :+ ch
             if (!numToCharCombs(i).contains(charListCandidate))
               numToCharCombs(i).add(charListCandidate)
           }
-        })
-      })
+        )
+      )
     }
 
     numToCharCombs
@@ -172,16 +172,15 @@ object Anagrams {
    *  and has no zero-entries.
    */
   def subtract(x: Occurrences, y: Occurrences): Occurrences = {
-    var xMap = x.toMap
+    var occurrencesMap = x.toMap
 
-    val xCharList = x.map(entry => entry._1)
+    val charList = x.map(e => e._1)
     y.foreach(chFreq =>
-      if (xCharList.contains(chFreq._1))
-//        if (xMap(chFreq._1) >= chFreq._2)
-          xMap = xMap.updated(chFreq._1, xMap(chFreq._1) - chFreq._2)
+      if (charList.contains(chFreq._1))
+          occurrencesMap = occurrencesMap.updated(chFreq._1, occurrencesMap(chFreq._1) - chFreq._2)
     )
 
-    xMap.filter(chFreq => chFreq._2 != 0).toList
+    occurrencesMap.filter(chFreq => chFreq._2 != 0).toList
   }
 
   /** Returns a list of all anagram sentences of the given sentence.
@@ -225,105 +224,90 @@ object Anagrams {
    *  Note: There is only one anagram of an empty sentence.
    */
 
-  //todo шаг 1 -
-  //получить все возможные упорядоченные(?) комбинации List[Occurrences] из словаря:
-  //Set[List[Occurrences]]
-  //List[Occurrences] - упорядочивать лексикографически, по длине Occurrences
-
-    //todo шаг 1.1 - посчитать combinations (упорядочивать их лексикографически, по длине Occurrences)
-    //todo шаг 1.2 - среди combinations найти те, которые есть в словаре
-    //обновить словарь этими значениями (в конце процедуры)
-    //todo шаг 1.3 - subtract эти комбинации, повторить процедуру для каждой ветви
-    //на каждой итерации проверять, что данное решение еще не содержится в дереве решиений
-    //например, (a,b) == (b,a) для Occurrences a,b, поэтому нет смысла рассматривать (b,a),
-    //если (a,b) уже есть в дереве решений
-
-  //todo шаг 2 -
-  // считать комбинации слов (в т.ч. перестановки слов в прелложении;
-  // учесть, что в предложении одно слово может встречаться больше одного раза)
-
+  /*шаг 1 -
+    получить все возможные упорядоченные комбинации List[Occurrences] из словаря
+  шаг 2 -
+    считать комбинации слов (в т.ч. перестановки слов в прелложении;
+    учесть, что в предложении одно слово может встречаться больше одного раза)*/
   def sentenceAnagrams(sentence: Sentence): List[Sentence] = {
+    //шаг 1
     if (sentence.isEmpty) return List(Nil)
 
-    val sntncOccurrences = sentenceOccurrences(sentence)
+    val occurrencesForSentence = sentenceOccurrences(sentence)
 
     val occurrencesCombinations = ListBuffer[List[Occurrences]]()
-    val input = mutable.Queue[(List[Occurrences], List[Occurrences], Occurrences)]((List[Occurrences](), dictionaryByOccurrences.toList.map(p => p._1), sntncOccurrences))
+    val queue = mutable.Queue[(List[Occurrences], List[Occurrences], Occurrences)] (
+      (List[Occurrences](), dictionaryByOccurrences.toList.map(p => p._1), occurrencesForSentence) )
 
-    while (input.nonEmpty) {
-      calcStep(input, occurrencesCombinations)
+    while (queue.nonEmpty) {
+      processNextQueueElement(queue, occurrencesCombinations)
     }
 
+    //шаг 2
     val result = ListBuffer[Sentence]()
-
     occurrencesCombinations.foreach(occurrencesList => result ++= occurrencesListToSentenceList(occurrencesList))
-
     result.toList
   }
 
-  //todo сделать перестановки слов в предложении. учесть, что могут быть два одинаковых слова
   def occurrencesListToSentenceList(occurrencesList: List[Occurrences]): List[Sentence] = {
-    var result1 = ListBuffer[Sentence](List())
-    var result2 = ListBuffer[Sentence]()
+    var sentenceList = List[Sentence](List())
 
+    //в sentenceList получаем все возможные комбинации слов для данного occurrencesList
     for (i <- occurrencesList.indices) {
       val wordList = dictionaryByOccurrences(occurrencesList(i))
-      result2 = ListBuffer[Sentence]()
-      for (j <- wordList.indices) {
-        result1.foreach(tempWordList => result2 += (tempWordList :+ wordList(j)))
-      }
-      result1 = result2
+      val sentenceSwapList = ListBuffer[Sentence]()
+      for (j <- wordList.indices)
+        sentenceList.foreach(tempWordList => sentenceSwapList += (tempWordList :+ wordList(j)))
+      sentenceList = sentenceSwapList.toList
     }
 
+    //делаем перестановки слов в каждом предложении
+    // (метод permutations учитывает, что могут быть два одинаковых элемента)
     val result = ListBuffer[Sentence]()
-    result2.foreach(sentence => result ++= permutateWordsInSentence(sentence))
+    sentenceList.foreach(sentence => result ++= sentence.permutations.toList)
     result.toList
   }
 
-  def permutateWordsInSentence(sentence: Sentence): List[Sentence] = {
-    val result = mutable.Set[Sentence](sentence)
-    sentence.permutations.foreach(s => result += s)
-    result.toList
-  }
 
-  def combinationsWithTails(occurrences: Occurrences, dictionary: List[Occurrences]): List[(Occurrences, Occurrences)] = {
-    var result = List[(Occurrences, Occurrences)]()
-
-    combinations(occurrences).foreach(occ => if (dictionary.contains(occ)) result :+= (occ, subtract(occurrences, occ)))
-
-    result
-  }
-
-  def calcStep(input: mutable.Queue[(List[Occurrences], List[Occurrences], Occurrences)], output: ListBuffer[List[Occurrences]]): Unit = {
-    if (input.isEmpty) return
+  /*
+  шаг 1 -
+    посчитать combinations
+  шаг 2 -
+    среди combinations найти те, которые есть в словаре; обновить словарь этими значениями
+  шаг 3 -
+    subtract эти комбинации, повторить процедуру для каждой ветви
+  Примечание:
+    При добавлении элемента в очередь или в конечный результат output проверять,
+    что данное решение еще там не содержится.
+    Например, (a,b) == (b,a) для Occurrences a,b, поэтому нет смысла рассматривать (b,a),
+    если (a,b) уже есть в списке */
+  def processNextQueueElement(input: mutable.Queue[(List[Occurrences], List[Occurrences], Occurrences)],
+                              output: ListBuffer[List[Occurrences]]): Unit = {
+    if (input.isEmpty)
+      return
 
     val first = input.dequeue
 
-    val preResult = first._1
+    val occurrencesCombinations = first._1
     val dictionary = first._2
-    val tail = first._3
+    val remainder = first._3
 
-    val combinationsPlusTails = combinationsWithTails(tail, dictionary)
+    val combinationsWithRemainders =
+      combinations(remainder).filter(o => dictionary.contains(o)).map(o => (o, subtract(remainder, o)))//шаг 1,2,3
 
-    //в словаре не нашлось слов для данного остатка tail
-    if (combinationsPlusTails.isEmpty) {
-//      recur(input, output)
+    //в словаре не нашлось слов для данного остатка remainder
+    if (combinationsWithRemainders.isEmpty)
       return
-    }
 
-    val newDictionary = combinationsPlusTails.map(e => e._1)
+    val newDictionary = combinationsWithRemainders.map(e => e._1)//шаг 2
 
-    combinationsPlusTails.foreach(e => {
-      val sortedList = sortOccurrencesList(preResult :+ e._1)
+    combinationsWithRemainders.foreach(e => {
+      val sortedList = sortOccurrencesList(occurrencesCombinations :+ e._1)
 
-      if (e._2.isEmpty && !output.contains(sortedList)) {
+      if (e._2.isEmpty && !output.contains(sortedList))
         output += sortedList
-      }
-      else if (e._2.nonEmpty && !input.map(e => e._1).contains(sortedList)) {
+      else if (e._2.nonEmpty && !input.map(e => e._1).contains(sortedList))
         input.enqueue((sortedList, newDictionary, e._2))
-      }
     })
-
-//    recur(input, output)
   }
 }
